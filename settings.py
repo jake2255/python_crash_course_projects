3c7b66f6493c4938e41e6eaeda70544597e0ac8f
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-06-16 18:45:21 -0500

    scoring increases as the difficulty increases

--- settings.py
+++ settings.py
@@ -9,10 +9,10 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # Ship settings
-        self.ship_limit = 2
+        self.ship_limit = 2 
 
         # Bullet settings
-        self.bullet_width = 3
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 6
@@ -22,6 +22,9 @@ class Settings:
         
         # How quickly the game speeds up
         self.speedup_scale = 1.50
+
+        # How quickly the alien point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -34,10 +37,13 @@ class Settings:
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
+        # Scoring settings
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
+        """Increase speed settings and alien point values."""
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
 
-    
+        self.alien_points = int(self.alien_points * self.score_scale)
